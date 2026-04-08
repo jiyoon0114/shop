abdb658304919dfb72e624f5a968ad733e1e8ef5
jiyoon0114 <greenpkh02@gmail.com>
2026-04-08 17:40:59 +0900

    260408 쇼핑몰 데이터 추가

--- js/entire-product.js
+++ js/entire-product.js
@@ -1,96 +1,69 @@
-import { product_clothes } from './product_data.js'
+import { product_clothes, product_glasses, product_Acc } from './product_data.js'
 
-const MainProductParent = document.querySelector(".product-box");
+const ClothesProductParent = document.querySelector(".product-box");
 
-// product_clothes.forEach(function(product) {
-//   const new_card = document.createElement('a');
-//   new_card.classList.add('product-card');
-//   new_card.classList.add('border-rad');
+const sortedView = document.querySelector('.orblist-order > :nth-child(1)');
+const sortedSelling = document.querySelector('.orblist-order > :nth-child(2)');
+const sortedPrice = document.querySelector('.orblist-order > :nth-child(3)');
 
-//   const new_card_img = document.createElement('img');
-//   new_card_img.src = product.img;
-//   new_card_img.alt = product.name;
-//   new_card_img.classList.add('border-rad');
+const clothesBtn = document.querySelector('.product-category > :nth-child(1)');
+const accbtn = document.querySelector('.product-category > :nth-child(2)');
+const glassesBtn = document.querySelector('.product-category > :nth-child(3)');
 
-//   const new_product_name = document.createElement('div');
-//   new_product_name.classList.add('product-name');
-//   new_product_name.textContent = product.name;
+let category = product_clothes // 보여줄 제품 카테고리 종류를 category로 설정함
+let orderby = 'view'; // 제품 정렬 방식 변수
 
-//   const new_product_tag = document.createElement('div');
-//   new_product_tag.classList.add('price-tag');
-//   new_product_tag.textContent = product.price;
-
-
-//   new_card.appendChild(new_card_img);
-//   new_card.appendChild(new_product_name);
-//   new_card.appendChild(new_product_tag);
-
-//   MainProductParent.appendChild(new_card);
-// });
-
-// function orbyChange_bn(element) { 
-//   // 여기서 element는 클릭된 <li> 태그 자체를 의미합니다. (HTML에서 보낸 'this')
-
-//   /* [단계 1] 청소하기 (Reset) */
-//   // 문서 전체에서 '.orblist-order' 안에 있는 모든 'li'를 다 찾아와서 'items'라는 변수에 담습니다.
-//   const items = document.querySelectorAll('.orblist-order li');
-
-//   // 찾아온 모든 li들을 하나씩 반복(forEach)하면서 검사합니다.
-//   items.forEach(li => {
-//     // 이미 'active' 클래스를 가진 녀석이 있다면 가차 없이 지워버립니다.
-//     // 이 과정이 없으면 클릭하는 족족 모든 항목에 색이 들어오게 됩니다. (하나만 선택 안 됨)
-//     li.classList.remove('active');
-//   });
-
-//   /* [단계 2] 강조하기 (Set) */
-//   // 이제 방금 클릭당한 그 녀석(element)에게만 'active'라는 클래스 이름을 붙여줍니다.
-//   // 그러면 아까 CSS에서 설정한 화려한 배경색과 그림자가 즉시 나타납니다.
-//   element.classList.add('active');
-
-//   const sortOption = element.textContent;
-
-//   if (sortOption == 'Most Viewed') {
-
-//   }
-//   else if (sortOption == 'Best Selling') {
-
-//   }
-//   else if (sortOption == 'Low to High') {
-
-//   }
-// }
-
-// window.orbyChange_bn = orbyChange_bn;
+//click 시 event 파라미터로 하는 orbyChange_sort함수 실행, 정렬 방식이 달라짐
+sortedView.addEventListener('click', (e) => {
+  orderby = 'view';
+  orbyChange_sort(e, 'orblist-order')
+});
+sortedSelling.addEventListener('click', (e) => {
+  orderby = 'selling';
+  orbyChange_sort(e, 'orblist-order')
+});
+sortedPrice.addEventListener('click', (e) => {
+  orderby = 'high';
+  orbyChange_sort(e, 'orblist-order')
+});
 
 
 
-// ------------------------------------------------------------------
-const child1 = document.querySelector('.orblist-order > :nth-child(1)');
-const child2 = document.querySelector('.orblist-order > :nth-child(2)');
-const child3 = document.querySelector('.orblist-order > :nth-child(3)');
+clothesBtn.addEventListener('click', (e) => {
+  category = product_clothes;
+  orbyChange_sort(e, 'product-category')
+});
+glassesBtn.addEventListener('click', (e) => {
+  category = product_glasses;
+  orbyChange_sort(e, 'product-category')
+});
+accbtn.addEventListener('click', (e) => {
+  category = product_Acc;
+  orbyChange_sort(e, 'product-category')
+});
 
 //완전 처음 초기에만 실행 하는 함수
-(() => {productListSort()})();
-(() => {orbyChange_bn2(child1)})();
+(() => {productListSort('view', category)})();
+(() => {orbyChange_bn2(sortedView, 'orblist-order')})();
+(() => {orbyChange_bn2(clothesBtn, 'product-category')})();
 
-child1.addEventListener('click', (e) => orbyChange_sort(e, 'view'));
-child2.addEventListener('click', (e) => orbyChange_sort(e, 'selling'));
-child3.addEventListener('click', (e) => orbyChange_sort(e, 'high'));
+//orderby에 따라서 기준을 다르게 데이터 정렬함
+function orbyChange_sort(e, parent) {
 
-function orbyChange_sort(e, orderby) {
-  orbyChange_bn2(e.target);
+  orbyChange_bn2(e.target, parent);
   
   if(orderby === 'view') {
-    productListSort('view');
+    productListSort('view', category);
   } else if(orderby === 'selling') {
-    productListSort('selling');
+    productListSort('selling', category);
   } else if(orderby === 'high') {
-    productListSort('high');
+    productListSort('high', category);
   }
 }
 
-function orbyChange_bn2(target) { 
-  const items = document.querySelectorAll('.orblist-order li');
+//target태그에 active 클라스 부여함
+function orbyChange_bn2(target, targetParent) { 
+  const items = document.querySelectorAll(`.${targetParent} li`);
 
   items.forEach(li => {
     li.classList.remove('active');
@@ -100,8 +73,8 @@ function orbyChange_bn2(target) {
 }
 
 //orderby = 'view' -> orderby의 초기 디폴트값을 view로 설정
-function productListSort(orderby = 'view') {
-  const cpList = [...product_clothes];
+function productListSort(orderby = 'view', catogory) {
+  const cpList = [...catogory];
 
   cpList.sort((item1, item2) => {
     let figure = 0;
@@ -118,8 +91,9 @@ function productListSort(orderby = 'view') {
   setProductList(cpList);
 }
 
+//정렬된 데이터를 보고 구조 다시 만들기
 function setProductList(list) {
-  MainProductParent.innerHTML = '';
+  ClothesProductParent.innerHTML = '';
 
   list.forEach(function(product) {
     const new_card = document.createElement('a');
@@ -144,6 +118,6 @@ function setProductList(list) {
     new_card.appendChild(new_product_name);
     new_card.appendChild(new_product_tag);
 
-    MainProductParent.appendChild(new_card);
+    ClothesProductParent.appendChild(new_card);
   });
 }
--- js/product_data.js
+++ js/product_data.js
@@ -1,19 +1,53 @@
-const productPictureAddress = '../img/products/'
+const productClothesPictureAddress = '../img/products/clothes/';
 
 export const product_clothes = [
-  { img: `${productPictureAddress}product1.jfif`, name: "상품명1", price: "40,000원", selling:5, view: 1, },
-  { img: `${productPictureAddress}product2.jfif`, name: "상품명2", price: "50,000원", selling:6, view: 1, },
-  { img: `${productPictureAddress}product3.jfif`, name: "상품명3", price: "70,000원", selling:8, view: 1, },
-  { img: `${productPictureAddress}product4.jfif`, name: "상품명4", price: "80,000원", selling:10, view: 1, },
-  { img: `${productPictureAddress}product5.jfif`, name: "상품명5", price: "90,000원", selling:40, view: 1, },
-  { img: `${productPictureAddress}product6.jfif`, name: "상품명6", price: "100,000원", selling:70, view: 1, },
-  { img: `${productPictureAddress}product7.jpg`,  name: "상품명7", price: "110,000원", selling:4, view: 1, },
-  { img: `${productPictureAddress}product8.jfif`, name: "상품명8", price: "120,000원", selling:111, view: 2, },
-  { img: `${productPictureAddress}product9.jpg`, name: "상품명9", price: "130,000원", selling:87, view: 1, },
-  { img: `${productPictureAddress}product10.jpg`, name: "상품명10", price: "130,000원", selling:64, view: 1, },
-  { img: `${productPictureAddress}product11.jpg`, name: "상품명11", price: "130,000원", selling:39, view: 1, },
-  { img: `${productPictureAddress}product12.jpg`, name: "상품명12", price: "130,000원", selling:87, view: 1, },
-  { img: `${productPictureAddress}product13.jpg`, name: "상품명13", price: "130,000원", selling:56, view: 1, },
-  { img: `${productPictureAddress}product14.jpg`, name: "상품명14", price: "130,000원", selling:99, view: 1, },
-  { img: `${productPictureAddress}product15.jpg`, name: "상품명15", price: "130,000원", selling:213, view: 1, }
+  { img: `${productClothesPictureAddress}product1.jfif`, name: "상품명1", price: "40,000원", selling:5, view: 1, },
+  { img: `${productClothesPictureAddress}product2.jfif`, name: "상품명2", price: "50,000원", selling:6, view: 1, },
+  { img: `${productClothesPictureAddress}product3.jfif`, name: "상품명3", price: "70,000원", selling:8, view: 1, },
+  { img: `${productClothesPictureAddress}product4.jfif`, name: "상품명4", price: "80,000원", selling:10, view: 1, },
+  { img: `${productClothesPictureAddress}product5.jfif`, name: "상품명5", price: "90,000원", selling:40, view: 1, },
+  { img: `${productClothesPictureAddress}product6.jfif`, name: "상품명6", price: "100,000원", selling:70, view: 1, },
+  { img: `${productClothesPictureAddress}product7.jpg`,  name: "상품명7", price: "110,000원", selling:4, view: 1, },
+  { img: `${productClothesPictureAddress}product8.jfif`, name: "상품명8", price: "120,000원", selling:111, view: 2, },
+  { img: `${productClothesPictureAddress}product9.jpg`, name: "상품명9", price: "130,000원", selling:87, view: 1, },
+  { img: `${productClothesPictureAddress}product10.jpg`, name: "상품명10", price: "130,000원", selling:64, view: 1, },
+  { img: `${productClothesPictureAddress}product11.jpg`, name: "상품명11", price: "130,000원", selling:39, view: 1, },
+  { img: `${productClothesPictureAddress}product12.jpg`, name: "상품명12", price: "130,000원", selling:87, view: 1, },
+  { img: `${productClothesPictureAddress}product13.jpg`, name: "상품명13", price: "130,000원", selling:56, view: 1, },
+  { img: `${productClothesPictureAddress}product14.jpg`, name: "상품명14", price: "130,000원", selling:99, view: 1, },
+  { img: `${productClothesPictureAddress}product15.jpg`, name: "상품명15", price: "130,000원", selling:213, view: 1, }
 ];
+
+const productGlassesPictureAddress = '../img/products/glasses/';
+
+export const product_glasses = [
+  { img: `${productGlassesPictureAddress}product1.jpg`, name: "상품명1", price: "40,000원", selling:5, view: 1, },
+  { img: `${productGlassesPictureAddress}product2.jpg`, name: "상품명2", price: "50,000원", selling:6, view: 1, },
+  { img: `${productGlassesPictureAddress}product3.jpg`, name: "상품명3", price: "70,000원", selling:8, view: 1, },
+  { img: `${productGlassesPictureAddress}product4.jpg`, name: "상품명4", price: "80,000원", selling:10, view: 1, },
+  { img: `${productGlassesPictureAddress}product5.jpg`, name: "상품명5", price: "90,000원", selling:40, view: 1, },
+  { img: `${productGlassesPictureAddress}product6.jpg`, name: "상품명6", price: "100,000원", selling:70, view: 1, },
+  { img: `${productGlassesPictureAddress}product7.jpg`,  name: "상품명7", price: "110,000원", selling:4, view: 1, },
+  { img: `${productGlassesPictureAddress}product8.jpg`, name: "상품명8", price: "120,000원", selling:111, view: 2, },
+  { img: `${productGlassesPictureAddress}product9.jpg`, name: "상품명9", price: "130,000원", selling:87, view: 1, },
+  { img: `${productGlassesPictureAddress}product10.jpg`, name: "상품명10", price: "130,000원", selling:64, view: 1, }
+]
+
+
+const productAccPictureAddress = '../img/products/accesories/';
+
+export const product_Acc = [
+  { img: `${productAccPictureAddress}product1.jpg`, name: "상품명1", price: "40,000원", selling:5, view: 1, },
+  { img: `${productAccPictureAddress}product2.jpg`, name: "상품명2", price: "50,000원", selling:6, view: 1, },
+  { img: `${productAccPictureAddress}product3.jpg`, name: "상품명3", price: "70,000원", selling:8, view: 1, },
+  { img: `${productAccPictureAddress}product4.jpg`, name: "상품명4", price: "80,000원", selling:10, view: 1, },
+  { img: `${productAccPictureAddress}product5.jpg`, name: "상품명5", price: "90,000원", selling:40, view: 1, },
+  { img: `${productAccPictureAddress}product6.jpg`, name: "상품명6", price: "100,000원", selling:70, view: 1, },
+  { img: `${productAccPictureAddress}product7.jpg`,  name: "상품명7", price: "110,000원", selling:4, view: 1, },
+  { img: `${productAccPictureAddress}product8.jpg`, name: "상품명8", price: "120,000원", selling:111, view: 2, },
+  { img: `${productAccPictureAddress}product9.jpg`, name: "상품명9", price: "130,000원", selling:87, view: 1, },
+  { img: `${productAccPictureAddress}product10.jpg`, name: "상품명10", price: "130,000원", selling:64, view: 1, },
+  { img: `${productAccPictureAddress}product11.jpg`, name: "상품명11", price: "130,000원", selling:39, view: 1, },
+  { img: `${productAccPictureAddress}product12.jpg`, name: "상품명12", price: "130,000원", selling:87, view: 1, },
+  { img: `${productAccPictureAddress}product13.jpg`, name: "상품명13", price: "130,000원", selling:56, view: 1, }
+]
